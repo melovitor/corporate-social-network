5224f8754ccb3d6724f2759d36f8c4098371b439
VitorM3L0 <vitormelobatista@gmail.com>
2022-12-12 23:44:18 -0300

    WIP - Modulo 1 - Iniciando com React.

---
+++ corporate-social-network/src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from './Post'
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author='Vitor de Melo'
+        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+      />
+    </div>
+
+  )
+}
+
