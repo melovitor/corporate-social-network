c12fdec6817037eaabf4945e0a9bac55b50a741b
VitorM3L0 <vitormelobatista@gmail.com>
2022-12-13 23:37:29 -0300

    WIP - Modulo 2 - CSS Modules.

--- corporate-social-network/src/App.jsx
+++ corporate-social-network/src/App.jsx
@@ -1,8 +1,11 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author='Vitor de Melo'
         content='Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
