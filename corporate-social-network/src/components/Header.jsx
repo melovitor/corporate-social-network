31b447077ab48377479f75a33508ef0705df34ac
VitorM3L0 <vitormelobatista@gmail.com>
2022-12-16 20:52:42 -0300

    WIP - Modulo 2 - Componente: Header.

--- corporate-social-network/src/components/Header.jsx
+++ corporate-social-network/src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
+import reactLogo from '../assets/react.svg'
+
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong >Corporate Social Network</strong>
+      <img src="/src/assets/react.svg" alt="Logotipo React" />
     </header>
 
   )
