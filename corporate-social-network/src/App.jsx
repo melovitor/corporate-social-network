9826c38e4cadda4b74dae28a5351f174285ca4db
VitorM3L0 <vitormelobatista@gmail.com>
2022-12-16 21:30:15 -0300

    WIP - Modulo 2 - Componente: Sidebar.

--- corporate-social-network/src/App.jsx
+++ corporate-social-network/src/App.jsx
@@ -1,15 +1,26 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
-import './global.css';
+
+import styles from './App.module.css'
+import './global.css'
+import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author='Vitor de Melo'
-        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
-      />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>
+          <Post
+            author='Vitor de Melo'
+            content='Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+          />
+        </main>
+
+      </div>
     </div>
   )
 }
