8c5de6e9e49faed1171f92aede8f8fe615ac9f40
VitorM3L0 <vitormelobatista@gmail.com>
2022-12-21 22:21:21 -0300

    WIP - Modulo 2 - Componente: Comment.

---
+++ corporate-social-network/src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://github.com/melovitor.png' />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Vitor Santos</strong>
+              <time
+                title='20 de Dezembro às 23:11h'
+                dateTime='2022-12-20 23:11:55'>
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title='Deletar comentário'></button>
+            <Trash size={20} />
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
