f4cbb5c13163642ee109c68295f09231429ad72d
VitorM3L0 <vitormelobatista@gmail.com>
2022-12-29 22:30:35 -0300

    WIP - Modulo 2 -Componente: Avatar.

--- corporate-social-network/src/components/Sidebar.jsx
+++ corporate-social-network/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilSimpleLine } from "phosphor-react"
+import { Avatar } from "./Avatar";
 import styles from './Sidebar.module.css';
 
 
@@ -8,7 +9,7 @@ export function Sidebar() {
       <img className={styles.cover}
         src="https://images.unsplash.com/photo-1633356122544-f134324a6cee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmVhY3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=50" />
       <div className={styles.profile}>
-        <img className={styles.avatar} src='https://github.com/melovitor.png' />
+        <Avatar src="https://github.com/melovitor.png" />
         <strong>Vitor de Melo</strong>
         <span>Web Developer</span>
       </div>
