fdcde4c0b71698546ffb300fcaca910533f9089d
VitorM3L0 <vitormelobatista@gmail.com>
2023-01-02 00:02:31 -0300

    WIP - Modulo 2 - Realizando likes nos comentários.

--- corporate-social-network/src/components/Comment.jsx
+++ corporate-social-network/src/components/Comment.jsx
@@ -1,11 +1,17 @@
-import { ThumbsUp, Trash } from 'phosphor-react';
+import { HandsClapping, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     onDeleteComment(content)
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1)
   }
 
 
@@ -30,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
-            <ThumbsUp size={20} />
-            Aplaudir <span>20</span>
+          <button onClick={handleLikeComment}>
+            <HandsClapping size={20} />
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
